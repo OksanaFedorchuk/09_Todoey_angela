03bfad6bcee15995f63a8b774e220dd8483932d3
Oksana Fedorchuk <klassi4eske@gmail.com>
2019-12-05 16:25:30 +0200

    2. Items model created and included in code. User defaults are working with simple array, but not working with Items model. Changing to NS coding.

---
+++ Todoey_Bootcamp/Controllers/TodoListViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Todoey_Bootcamp
+//
+//  Created by Oksana on 25.11.2019.
+//  Copyright © 2019 Oksana Fedorchuk. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let newItem = Item()
+        newItem.title = "Oksana"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Dima"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Olia"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
+    }
+    
+    
+    //MARK - Tableview datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = itemArray[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = item.title
+        
+        //ternary operator:
+        //value = condition ? valueTrue : valueFalse
+        cell.accessoryType =  item.done ? .checkmark : .none
+        //the line above is equal to the commented code below->
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        return cell
+    }
+    
+    //MARK - Tableview delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //the line above is equal to the commented code below->
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+//
+        tableView.reloadData()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "New todo", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) {(action) in
+            //what will happen when the user clicks the Add button on UIAlert
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new todo"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+}
+
---
+++ Todoey_Bootcamp/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey_Bootcamp
+//
+//  Created by Oksana on 05.12.2019.
+//  Copyright © 2019 Oksana Fedorchuk. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
