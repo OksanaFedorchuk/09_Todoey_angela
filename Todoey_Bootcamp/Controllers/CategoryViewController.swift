2225e5da910c1537b4a55bd94f126c311b66a968
Oksana Fedorchuk <klassi4eske@gmail.com>
2019-12-18 21:41:09 +0200

    10. Added the second tableviewcontroller.

---
+++ Todoey_Bootcamp/Controllers/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Todoey_Bootcamp
+//
+//  Created by Oksana on 18.12.2019.
+//  Copyright © 2019 Oksana Fedorchuk. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
